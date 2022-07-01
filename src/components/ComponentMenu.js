e2674fc90ea5475e6bb702ddb738307117806237
Javier160516 <javiercambranis23@gmail.com>
2022-07-01 14:08:06 -0500

    .

--- src/components/ComponentMenu.js
+++ src/components/ComponentMenu.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import {View} from 'react-native'
+import { View, Text } from 'react-native'
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer'
 import Theme from '../Theme/Theme'
+import { Avatar } from 'react-native-paper'
+import DetectarTema from '../helpers/DetectarTema';
 const ComponentMenu = (props) => {
+    console.log(props);
+    const { themeTextStyle, themeContainerStyle } = DetectarTema();
     return (
-        <View style={Theme.styles.flex1}>
-            <DrawerContentScrollView {...props} 
-            contentContainerStyle={{backgroundColor: Theme.colors.azul}}
+        <View style={[Theme.styles.flex1, themeContainerStyle]}>
+            <View style={[Theme.styles.alignCenter, Theme.styles.pt80, Theme.styles.pb40, { backgroundColor: Theme.colors.azul }]}>
+                <Avatar.Image size={100} source={require('../img/pruebaface.jpg')} />
+                <Text style={[Theme.styles.fs20, Theme.colors.dark.darkText]}>Persona prueba</Text>
+            </View>
+            <DrawerContentScrollView {...props}
+                contentContainerStyle={[themeContainerStyle, { paddingTop: 0 }]}
             >
-                <View style={{flex: 1, backgroundColor: Theme.colors.blanco, paddingTop: 10}}>
                 <DrawerItemList {...props} />
-                </View>
             </DrawerContentScrollView>
         </View>
     )
